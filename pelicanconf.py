db8d7369838c909c75b02d54e2d86c3735f91209
Vadim Markovtsev <v.markovtsev@samsung.com>
2015-06-01 19:18:42 +0300

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Veles Team'
+SITENAME = "Veles Developers' Blog"
+SITEURL = 'https://velesnet.ml/blog'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+MD_EXTENSIONS = ["gfm"]
+
+# Blogroll
+LINKS = (('Veles on GitHub', 'https://github.com/samsung/veles'),
+         ('Gerrit', 'https://velesnet.ml/gerrit'),
+         ('Jenkins', 'http://velesnet.ml/jenkins'),
+         ('Benchmarks', 'http://velesnet.ml/benchmarks'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https:/twotter.com/velesml'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
